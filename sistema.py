9961a191a5babe7ceca7f4f16829548adefdb11d
Letícia Guimarães Coelho <leehcoelhoo@gmail.com>
2023-08-22 12:35:10 -0300

    Atualizando repositório

--- sistema.py
+++ sistema.py
@@ -1,17 +1,19 @@
 menu = """
+           MENU
+___________________________
 
 [0] Depositar
 [1] Sacar
 [2] Extrato
 [3] Sair
+___________________________
 
-=> """
+Escolha a opção desejada: """
 
 saldo = 0
-limite = 500
+limite = 1000
 extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
+cont = 0
 
 while True:
 
@@ -34,21 +36,21 @@ while True:
 
         excedeu_limite = valor > limite
 
-        excedeu_saques = numero_saques >= LIMITE_SAQUES
+        excedeu_saques = cont >= 3
 
         if excedeu_saldo:
-            print("Operação falhou! Você não tem saldo suficiente.")
+            print("\nOperação falhou! Você não tem saldo suficiente.")
 
         elif excedeu_limite:
-            print("Operação falhou! O valor do saque excede o limite.")
+            print("\nOperação falhou! O valor do saque excede o limite.")
 
         elif excedeu_saques:
-            print("Operação falhou! Número máximo de saques excedido.")
+            print("\nOperação falhou! Número máximo de saques excedido.")
 
         elif valor > 0:
             saldo -= valor
             extrato += f"Saque: R$ {valor:.2f}\n"
-            numero_saques += 1
+            cont +=1
 
         else:
             print("Operação falhou! O valor informado é inválido.")
@@ -63,4 +65,4 @@ while True:
         break
 
     else:
-        print("Operação inválida, por favor selecione novamente a operação desejada.")
+        print("\nOperação inválida, por favor selecione novamente a operação desejada.")
